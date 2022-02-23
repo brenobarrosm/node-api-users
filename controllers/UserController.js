420d7780299551e6e02079b0e5f766b33107dd36
Breno de Barros <brenowbarros21@gmail.com>
2022-02-23 19:14:47 -0400

    Salvando dados de novos usuarios no banco

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,23 @@
+var User = require("../models/User");
+
 class UserController {
     async index(req, res) { }
 
     async create(req, res) {
         var { name, email, password } = req.body;
-        if(email == undefined) {
+        if(email == undefined || email == ' ') {
             res.status(400);
             res.json({ err: "Email inválido" });
+            return;
         }
+        if(password == undefined || password == ' ') {
+            res.status(400);
+            res.json({ err: "Senha inválido" });
+            return;
+        }
+
+        await User.new(email, password, name);
+
         res.status(200);
         res.send("OK");
     }
